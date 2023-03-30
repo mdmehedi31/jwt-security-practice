38e798551656c0931196e5327d8fd9c72bab685d
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2023-03-30 20:00:34 +0600

    Jwt Authentication Filter added

--- src/main/java/com/spring/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/spring/security/config/JwtAuthenticationFilter.java
@@ -1,13 +1,18 @@
 package com.spring.security.config;
 
 
+import com.spring.security.services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,7 +23,9 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
 
+    private final UserDetailsService userDetailsServices;
     @Override
     protected void doFilterInternal(
            @NonNull HttpServletRequest request,
@@ -28,12 +35,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         final String authHeader= request.getHeader("Authorization");
         final String jwt;
-
+        final String userEmail;
         if(authHeader==null || authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request,response);
             return;
         }
 
+        jwt= authHeader.substring(7);
+        userEmail=jwtService.extractUsername(jwt);
 
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails= this.userDetailsServices.loadUserByUsername(userEmail);
+        }
     }
 }
