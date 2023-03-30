1e74315074d15d3f14551dd8e77cdc8391f00523
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2023-03-30 12:19:21 +0600

    user and basic filter added

---
+++ src/main/java/com/spring/security/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.spring.security.Repository;
+
+import com.spring.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/spring/security/enums/Role.java
@@ -0,0 +1,7 @@
+package com.spring.security.enums;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
